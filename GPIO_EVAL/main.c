aa383e156e3f04ab51754a673b822ef9cb6cdbca
Sitthipong <sitthipong.ven@gmail.com>
2019-03-21 11:49:09 +0700

    add GPIO for eval

---
+++ GPIO_EVAL/main.c
@@ -0,0 +1,131 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32L1xx_StdPeriph_Templates/main.c 
+  * @author  MCD Application Team
+  * @version V1.2.0
+  * @date    16-May-2014
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup Template_Project
+  * @{
+  */
+
+/* Private setup functions ---------------------------------------------------------*/
+void RCC_setup(void);
+void GPIO_setup(void);
+
+/* Private user define functions ---------------------------------------------------------*/
+void delay(unsigned long ms);
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  RCC_setup();
+  GPIO_setup();
+  while (1)
+  {
+    /* LED at PB9 ON */
+		GPIO_SetBits(GPIOB,GPIO_Pin_9);
+		/* Delay 0.5 sec */
+		delay(500);
+		/* LED at PB9 OFF */
+		GPIO_ResetBits(GPIOB,GPIO_Pin_9);
+		/* Delay 0.5 sec */
+		delay(500);
+  }
+}
+
+/*---------------------Function----------------------------*/
+void RCC_setup(void)
+{
+  /* RCC system reset(for debug purpose) */
+  RCC_DeInit();
+  /* Enable Internal Clock HSI */
+  RCC_HSICmd(ENABLE);
+  /* Wait till HSI is Ready */
+  while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY)==RESET);
+  RCC_HCLKConfig(RCC_SYSCLK_Div1);
+  RCC_PCLK1Config(RCC_HCLK_Div2);
+  RCC_PCLK2Config(RCC_HCLK_Div2);
+  FLASH_SetLatency(FLASH_Latency_0);
+  /* Enable PrefetchBuffer */
+  FLASH_PrefetchBufferCmd(ENABLE);
+  /* Set HSI Clock Source */
+  RCC_SYSCLKConfig(RCC_SYSCLKSource_HSI);
+  /* Wait Clock source stable */
+  while(RCC_GetSYSCLKSource()!=0x00);
+}
+
+void GPIO_setup(void)
+{
+	/* GPIO Sturcture */
+	GPIO_InitTypeDef GPIO_InitStructure;
+	/* Enable Peripheral Clock AHB for GPIOB */
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB,ENABLE);
+	/* Configure PC13 as Output push-pull */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_40MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
+// delay 1 ms per count @ Crystal 16.0 MHz 
+void delay(unsigned long ms)
+{
+	volatile unsigned long i,j;
+	for (i = 0; i < ms; i++ )
+	for (j = 0; j < 1227; j++ );
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+// not sure 1ms/(1/Clock)*0.7675 - 1
